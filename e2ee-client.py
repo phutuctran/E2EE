cf2f5f4c0bd70fb123f5ad9a54da8b9d0f15e727
dangthuanvo <20520314@gm.uit.edu.vn>
2023-10-25 20:17:08 +0700

    innit

---
+++ e2ee-client.py
@@ -0,0 +1,59 @@
+import socket
+import threading
+import rsa
+from flask import Flask, render_template, request, redirect, session, url_for
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "e2ee123"
+PORT = 9999
+server_ip = "172.17.23.30"
+socketio = SocketIO(app)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+public_key, private_key = rsa.newkeys(1024)
+public_partner = None
+myname = None
+
+def connect_to_host():
+    client.connect((server_ip, PORT))
+    client.send(public_key.save_pkcs1(format='PEM'))
+    client.send(myname.encode())
+
+def receiving_messages():
+    global client, public_partner, private_key
+    while True:
+        if(public_partner == None):
+            public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+        else:
+            message = rsa.decrypt(client.recv(1024), private_key).decode()
+            print (message)
+
+def sending_messages():
+    global client, public_partner, myname
+    while True:
+        message = input("")
+        client.send(rsa.encrypt(f"{myname}: {message}".encode(), public_partner))
+
+@app.route("/", methods = ["POST", "GET"])
+def index():
+    global client, myname
+    if request.method == "POST":
+        name = request.form.get("name")
+        if not name:
+            return render_template("index.html", error="Please enter a name.", name=name)
+        
+
+        myname = name
+        connect_to_host()
+        receivie_thread = threading.Thread(target=receiving_messages, args=()).start()
+        send_thread = threading.Thread(target=sending_messages, args=()).start()
+
+
+
+
+        return render_template("room.html", code=None)
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    socketio.run(app,port = 5000, debug=True)
---
+++ e2ee-server.py
@@ -0,0 +1,77 @@
+import json
+import socket
+import threading
+import rsa
+import requests
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+clientcount = 0
+client1 = None
+client2 = None
+client1_name = None
+client1_key = None
+client2_name = None
+client2_key = None
+message_count = 0
+
+def handle_message(client_socket):
+    global message_count, client1, client2
+    while True:
+        if(message_count == 4):
+            try:    
+                message = client_socket.recv(1024)
+                print(f"{message} received from client")
+                if not message:
+                    break
+                # Forward the message to other clients
+                if(client_socket == client1):
+                    client2.send(message)
+                else:
+                    client1.send(message)
+
+            except Exception as e:
+                print(f"Error: {str(e)}")
+                break
+
+
+def handle_connect(client_socket):
+    global clientcount, client1_name, client1_key, client2_name, client2_key, client1, client2, message_count, client1, client2
+    if(clientcount == 0):
+        clientcount += 1
+        client1 = client_socket
+        client1_key = rsa.PublicKey.load_pkcs1(client_socket.recv(1024))
+        client1_name = client_socket.recv(1024)
+    else:
+        client2 = client_socket
+        client2_key = rsa.PublicKey.load_pkcs1(client_socket.recv(1024))
+        client2_name = client_socket.recv(1024)
+        client_message1 = threading.Thread(target=handle_message, args=(client1,)).start()
+        client_message2 = threading.Thread(target=handle_message, args=(client2,)).start()
+        client1.send(client2_key.save_pkcs1(format='PEM'))
+        client2.send(client1_key.save_pkcs1(format='PEM'))
+    message_count += 2
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((get_ip(), 9999))
+server.listen()
+
+print("Your IPv4 address is:", get_ip())
+while True:
+    client_socket, client_addr = server.accept()
+    print(f"Accepted connection from {client_addr[0]}:{client_addr[1]}")
+    # Create a new thread to handle the client
+    client_connect = threading.Thread(target=handle_connect, args=(client_socket,))
+    client_connect.start()
+
+
